b913075d19d13ae9dcbfef7de98b8434ece29384
FearosD <fearos@valar.ru>
2026-09-24 12:31:27 +0300

    init

---
+++ email-constructor/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <!-- Здесь будет главный редактор -->
+    <p>Email Constructor initialized.</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+/* Минимальные сбросы стилей */
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
